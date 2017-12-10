dd0dd3c05597a4b55cb0a585763de975480de2ae
unknown <jtc7537@gmail.com>
2017-12-10 14:56:21 -0500

    nested guess tracking inside of word creation function

--- game.js
+++ game.js
@@ -7,23 +7,40 @@ var wins = document.getElementById("wins");
 wins.textContent = winCount;
 
 
-function randomWord(event.key) {
+document.onkeyup = function(event) {
+//	if (event.key === )
+
+	// Array of random words
 	var wordList = ["nachos","pizza","pasta","tacos","thai","tots"];
 
+
+	// chooses random word based on index number in array
 	var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 
+	// assigns the variable word to the id "hidden-word"
 	var word = document.getElementById("hidden-word");
       
-    word.innerHTML = randomWord.split('');
-}
+    // set text for the current word field equal to the random word and turns it into a string  
+    word.innerHTML = randomWord.split('');  
 
-function letterGuess(event.key) {
-	var remainingGuesses = document.getElementById("remaining-guesses");
-	remainingGuesses.textContent -= remainingGuessesTracker;
+    // assigns the variable alreadyGuesses to the guessed letters
+    var alreadyGuessed = document.getElementById("guessed-letters");
 
-	var alreadyGuessed = document.getElementById("guessed-letters");
-	alreadyGuessed.innerHTML += event.key.split();
+    document.onkeyup = function(event) {
+
+	    if ((randomWord.indexOf(event.key) === -1)) {
+
+		    alreadyGuessed.innerHTML += event.key;
+
+			var remainingGuesses = document.getElementById("remaining-guesses");
+			remainingGuesses.textContent = remainingGuessesTracker;
+
+	    }
+	}
+	
+
+	
 } 
-
+ 
 
  
