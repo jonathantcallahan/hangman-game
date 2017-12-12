2bfca7db12a1a66e924ba39ede4362ea7b9a6aa0
unknown <jtc7537@gmail.com>
2017-12-11 19:37:02 -0500

    created underscore placeholders for word

--- game.js
+++ game.js
@@ -23,30 +23,53 @@ function resetScore() {
 
 	var wordList = ["nachos","pizza","pasta","tacos","thai","tots"];
 	var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
-	var word = document.getElementById("hidden-word"); 
-    word.innerHTML = randomWord.split(''); 
+
+	var underscoreArray = [];
+
+	for (var i = 0; i < wordList.length; i++) {
+		underscoreArray.push("_");
+	}  
+
+	var wordPlaceHolder = document.getElementById("hidden-word"); 
+    wordPlaceHolder.innerHTML = underscoreArray; 
 }
 
 document.onkeyup = function(event) {
 //	if (event.key === )
 
 	// Array of random words
-	var wordList = ["nachos","pizza","pasta","tacos","thai","tots"];
+	//var wordList = ["nachos","pizza","pasta","tacos","thai","tots"];
 
 
 	// chooses random word based on index number in array
-	var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+	//var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 
 	// assigns the variable word to the id "hidden-word"
-	var word = document.getElementById("hidden-word");
+	//var word = document.getElementById("hidden-word");
       
     // set text for the current word field equal to the random word and turns it into a string  
-    word.innerHTML = randomWord.split('');  
+    //word.innerHTML = randomWord.split('');  
   
 
     //var guesses = [];
 
     //alreadyGuessed.innerHTML = guesses;
+
+    //NEW RANDOM WORD PLACEHOLDER GENERATOR
+
+    	var wordList = ["nachos","pizza","pasta","tacos","thai","tots"];
+	var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+
+	var underscoreArray = [];
+
+	for (var i = 0; i < wordList.length; i++) {
+		underscoreArray.push("_");
+	}  
+
+	var wordPlaceHolder = document.getElementById("hidden-word"); 
+    wordPlaceHolder.innerHTML = underscoreArray;
+
+    //END NEW RANDOM WORD PLACEHOLDER GENERATOR
 
     alreadyGuessed.textContent = event.key;
 
@@ -68,6 +91,10 @@ document.onkeyup = function(event) {
 			if (remainingGuessesTracker === 0) {
 				resetScore();
 			}
+
+			if (randomWord.indexOf(event.key) > -1) {
+				underscoreArray[randomWord.indexOf(event.key)] = event.key;
+			}	
 
 		}
 
